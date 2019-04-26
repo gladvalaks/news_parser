e91721ee6c66a8ffa60fbc867258cc81d8db4a57
carlmarks203 <45235909+carlmarks203@users.noreply.github.com>
2019-04-26 15:25:56 +0300

    Теперь могёт с лентача и гулага

--- test.py
+++ test.py
@@ -4,8 +4,17 @@ import pygame
 import os
 import pprint
 
-def get_post(num):
-    req = 'https://api.vk.com/method/wall.get'
-    response = requests.get('https://api.vk.com/method/wall.get?owner_id=-112510789&count=20&access_token=b346d612b346d612b346d61288b32cfdcebb346b346d612eff9ea604f19d70c363f4e13&v=5.95')
-    text = response.json()['response']['items'][num]['text']
+def get_post(num, group):
+    if group == 1:
+        req = 'https://api.vk.com/method/wall.get'
+        response = requests.get('https://api.vk.com/method/wall.get?owner_id=-112510789&count=20&access_token=b346d612b346d612b346d61288b32cfdcebb346b346d612eff9ea604f19d70c363f4e13&v=5.95')
+        text = response.json()['response']['items'][num]['text']
+    if group == 2:
+        req = 'https://api.vk.com/method/wall.get'
+        response = requests.get('https://api.vk.com/method/wall.get?owner_id=-29534144&count=20&access_token=b346d612b346d612b346d61288b32cfdcebb346b346d612eff9ea604f19d70c363f4e13&v=5.95')
+        text = response.json()['response']['items'][num]['text']
+    if group == 3:
+        req = 'https://api.vk.com/method/wall.get'
+        response = requests.get('https://api.vk.com/method/wall.get?owner_id=-145037861&count=20&access_token=b346d612b346d612b346d61288b32cfdcebb346b346d612eff9ea604f19d70c363f4e13&v=5.95')
+        text = response.json()['response']['items'][num]['text']
     return text
